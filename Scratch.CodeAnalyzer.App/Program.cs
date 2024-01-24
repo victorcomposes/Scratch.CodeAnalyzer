55b0bdb5b1693e83f29de274409a35c32fee6caf
Victor I <e7victor@gmail.com>
2024-01-24 18:49:01 +0200

    first commit

---
+++ Scratch.CodeAnalyzer.App/Program.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Scratch.CodeAnalyzer.Sdk;
+
+{
+    var person = Person.Create(null, null, null, null, null);
+}
---
+++ Scratch.CodeAnalyzer.Sdk/Person.cs
@@ -0,0 +1,40 @@
+﻿namespace Scratch.CodeAnalyzer.Sdk
+{
+    public class Person
+    {
+        private Person(
+            string id,
+            string name,
+            string lastName,
+            string title,
+            string city)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Title = title;
+            City = city;
+        }
+
+        public static Person Create(
+            string id,
+            string name,
+            string lastName,
+            string title,
+            string city)
+        {
+            return new Person(
+                id,
+                name,
+                lastName,
+                title,
+                city);
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string City { get; set; }
+    }
+}
